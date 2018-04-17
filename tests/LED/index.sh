253da5f2d3588b692d0b1f016b4156f20d9cb1ce
Liu Wenlong <liuwl.fnst@cn.fujitsu.com>
2018-04-17 10:22:09 -0400

    LED: testcases improvements
    About the details for each change points, please check
    the review comments of this commit on github.com.
    
    Signed-off-by: Liu Wenlong <liuwl.fnst@cn.fujitsu.com>

--- tests/LED/index.sh
+++ tests/LED/index.sh
@@ -4,12 +4,25 @@
 set -e
 #set -x
 echo "This is program led to check led light up or led light off"
-sleep 1
-for path in "/sys/class/leds/"*"/brightness" ;do
-	echo "\n$path +ON"
-	echo 1 > $path && echo "PASSED" || echo "FAILED"
-	sleep 2
-	echo "$path -OFF"
-	echo 0 > $path && echo "PASSED" || echo "FAILED"
-	sleep 2
-done
+
+LEDS_NUM=0
+if [ ! -f "/sys/class/leds/" ]; then
+	echo "CONF: /sys/class/leds not exist."
+	exit 1
+else
+	LEDS_NUM=ls /sys/class/leds/ | wc -l
+fi
+
+if [ $LEDS_NUM -gt 0 ]; then
+	sleep 1
+	for path in "/sys/class/leds/"*"/brightness" ;do
+		echo "\n$path +ON"
+		echo 1 > $path && echo "PASSED" || echo "FAILED"
+		sleep 2
+		echo "$path -OFF"
+		echo 0 > $path && echo "PASSED" || echo "FAILED"
+		sleep 2
+	done
+else
+	echo "CONF: nothing exist in /sys/class/leds/."
+fi
