68f5aabdf1e4364927080dc311a942b9e785c1b8
Liu Wenlong <liuwl.fnst@cn.fujitsu.com>
2018-04-17 10:26:14 -0400

    IIC: testcases improvements
    About the details for each change points, please check
    the review comments of this commit on github.com.
    
    Signed-off-by: Liu Wenlong <liuwl.fnst@cn.fujitsu.com>

--- tests/IIC/013_i2c_unbind_bind.sh
+++ tests/IIC/013_i2c_unbind_bind.sh
@@ -6,18 +6,14 @@ set -e
 
 echo "\n************************I2C UNBIND BIND DEVICE TEST*********************\n"
 
+# First, unbind those devices which attached on the i2c bus.
+[ -f /sys/bus/platform/devices/e6ef0000.video/driver/unbind ] && echo e6ef0000.video > /sys/bus/platform/devices/e6ef0000.video/driver/unbind
+[ -f /sys/bus/platform/devices/feb00000.display/driver/unbind ] && echo feb00000.display > /sys/bus/platform/devices/feb00000.display/driver/unbind
+[ -f /sys/bus/platform/devices/ec500000.sound/driver/unbind ] && echo ec500000.sound > /sys/bus/platform/devices/ec500000.sound/driver/unbind
+
 if ! $(dirname $0)/../common/unbind-bind.sh "$DRIVER" "$I2C_INTERRUPT_1";then
     eval $FAIL_MEG
     exit 1
-fi
-
-cmd="aplay $(dirname $0)/data/test.wav"
-echo $cmd
-sleep 1
-
-if ! eval $cmd;then
-	eval $FAIL_MEG
-	exit 1
 fi
 
 eval $PASS_MEG
--- tests/IIC/config_i2c.sh
+++ tests/IIC/config_i2c.sh
@@ -3,21 +3,6 @@
 
 . $(dirname $0)/../../boards-conf/def_configs
 
-#DRIVER="i2c-rcar"
-#DRIVER_PATH="IIC"
-
-#i2c interrupt
-#I2C_INTERRUPT_1="e6510000.i2c"
-#I2C_INTERRUPT_2="e66d8000.i2c"
-
-#device i2c use
-#AK4643_NAME="ak4613"
-#AK4643_DEVICE="2-0010"
-#CS2000_NAME="cs2000-cp"
-#CS2000_DEVICE="2-004f"
-
-#device node
-#I2C_DEVICE_1="/dev/i2c-2"
-#I2C_DEVICE_2="/dev/i2c-4"
-
-#LOG_FILE="i2c.txt"
+DRIVER="i2c-rcar"
+DRIVER_PATH="IIC"
+LOG_FILE="i2c.txt"
